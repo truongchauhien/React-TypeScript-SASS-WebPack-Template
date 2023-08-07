ca482726b3376600d734ca4f9cf37adee8d7b16a
truongchauhien <truongchauhien@gmail.com>
2023-08-07 11:31:50 +0700

    Add redux toolkit, and react router.

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,22 @@
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from '../node_modules/react-router-dom/dist/index';
 import stylesheets from './app.module.scss';
 import './app.scss';
-import React from 'react';
+import Root from './features/layout/root';
 
-const App = <div className={stylesheets.app}></div>
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />
+    }
+])
+
+const App = (
+    <React.StrictMode>
+        <div className={stylesheets.app}>
+            <RouterProvider router={router} />
+        </div>
+    </React.StrictMode>
+)
 
 export default App;
